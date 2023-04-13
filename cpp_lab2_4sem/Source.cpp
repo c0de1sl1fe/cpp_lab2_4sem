dcc32a7f9df0cb890a557a26bac5c849e4cb4634
Yury <everydaysmok@gmail.com>
2023-04-13 20:42:36 +0400

    Add project files.

---
+++ cpp_lab2_4sem/Source.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+
+
+
+void PrintMenu()
+{
+    std::cout << "Hello, here your menu, choose one:" << std::endl;
+    std::cout << "1. Insert_sort" << std::endl;
+    std::cout << "2. Quick_sort " << std::endl;
+    std::cout << "3. Check stats" << std::endl;
+    std::cout << "4. Exit" << std::endl;
+    std::cout << "choice: ";
+}
+void main()
+{
+
+}
