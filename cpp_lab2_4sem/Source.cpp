6ff34a1d731fbdf7dfaf16b95cb86309df5529e3
Yury <everydaysmok@gmail.com>
2023-04-13 20:47:09 +0400

    Initialization

--- cpp_lab2_4sem/Source.cpp
+++ cpp_lab2_4sem/Source.cpp
@@ -3,7 +3,7 @@
 
 
 
-void PrintMenu()
+void printMenu()
 {
     std::cout << "Hello, here your menu, choose one:" << std::endl;
     std::cout << "1. Insert_sort" << std::endl;
@@ -12,7 +12,78 @@ void PrintMenu()
     std::cout << "4. Exit" << std::endl;
     std::cout << "choice: ";
 }
+int EnterNumber()
+{
+    double tmp;
+    char stmp[24];
+    bool exit1 = false;
+    while (!exit1)
+    {
+        bool exit = true;
+        gets_s(stmp);
+        char* endptr = stmp;
+        tmp = strtod(stmp, &endptr);
+        if (endptr == stmp)
+        {
+            exit = false;
+            continue;
+        }
+        if ((tmp == DBL_MAX || tmp == DBL_MIN) && errno == ERANGE)
+        {
+            exit = false;
+            continue;
+        }
+        while (*endptr)
+        {
+            if ((*endptr != ' ') && (*endptr != '\n'))
+            {
+                exit = false;
+                break;
+            }
+            endptr++;
+        }
+        if (exit)
+        {
+            exit1 = true;
+        }
+        else
+        {
+            exit1 = false;
+            printf("\nsomething wrong with number\n");
+        }
+    }
+    return (int)tmp;
+}
 void main()
 {
-
+    int choice = 0;
+    bool exit = false;
+    while (!exit)
+    {
+        printMenu();
+        choice = EnterNumber();
+        switch (choice)
+        {   
+        case(1):
+        {
+            break;
+        }
+        case(2):
+        {
+            break;
+        }
+        case(3):
+        {
+            break;
+        }
+        case(4):
+        {
+            exit = true;
+            break;
+        }
+        default:
+            std::cout << "Unable to recognize your request" << std::endl;
+            break;
+        }
+    }
 }
